f7382c15a8ef34fabcb287b266d8fd5d2dad2a5f
MihaKon <konon.michal@gmail.com>
2025-09-18 23:03:37 +0200

    adds tailwind package

--- app/main.py
+++ app/main.py
@@ -1,16 +1,26 @@
 import pathlib
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form, UploadFile, File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
-app = FastAPI()
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.gzip import GZipMiddleware
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 TEMPLATES_DIR = BASE_DIR / "templates"
+STATIC_DIR = BASE_DIR / "static"
 TEMPLATES = Jinja2Templates(directory=TEMPLATES_DIR)
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
+app.add_middleware(GZipMiddleware)
 
 
 @app.get("/", response_class=HTMLResponse)
-async def root() -> HTMLResponse:
-    return TEMPLATES.TemplateResponse("index.html", {"request": {}})
+async def root(request: Request) -> HTMLResponse:
+    return TEMPLATES.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/uploadfile/")
+async def upload_file(file: UploadFile):
+    return {"filename": file.filename}
