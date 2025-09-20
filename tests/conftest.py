321c0fafc657e88974921ff22e3ad99991f7a8ef
MihaKon <konon.michal@gmail.com>
2025-09-20 23:23:53 +0200

    Fix missing annotations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,18 @@
-import pytest
 from io import BytesIO
+
+import pytest
 from fastapi.testclient import TestClient
 
 from app.main import app
 
 
 @pytest.fixture
-def client():
-    """FastAPI test client fixture."""
+def client() -> TestClient:
     return TestClient(app)
 
 
 @pytest.fixture
 def sample_file() -> BytesIO:
-    """Sample file fixture for upload tests."""
     f = b"Hello World"
     byte_stream_file = BytesIO(f)
     byte_stream_file.name = "TestFileName.TestExtension"
