4ad54830edf1b0518374cb8e84a8efa7fdba0e0f
MihaKon <konon.michal@gmail.com>
2025-09-22 17:28:22 +0200

    Adds cif, pdb handlers, adds htmx,

---
+++ app/coarse_modeler.py
@@ -0,0 +1,5 @@
+from Bio.PDB.Structure import Structure
+
+
+def transform_to_coarse_grain(original_structure: Structure) -> Structure:
+    return original_structure
--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import pathlib
 from enum import Enum
 from io import StringIO
-from typing import Any
 
 import uvicorn
+from Bio.PDB.MMCIFParser import MMCIFParser
 from Bio.PDB.PDBParser import PDBParser
 from Bio.PDB.Structure import Structure
 from fastapi import FastAPI, HTTPException, Request, UploadFile
@@ -11,6 +11,8 @@ from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from app import coarse_modeler
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent
 TEMPLATES_DIR = BASE_DIR / "templates"
@@ -23,6 +25,9 @@ class SupportedFormats(Enum):
     CIF = "cif"
 
 
+FORMAT_PARSERS = {SupportedFormats.PDB: PDBParser, SupportedFormats.CIF: MMCIFParser}
+
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 app.add_middleware(GZipMiddleware)
@@ -32,7 +37,7 @@ app.add_middleware(GZipMiddleware)
 async def root(request: Request) -> HTMLResponse:
     return TEMPLATES.TemplateResponse(
         request=request,
-        name="index.html",
+        name="file_upload.html",
         context={
             "supported_file_formats": [
                 file_format.value for file_format in SupportedFormats
@@ -42,22 +47,35 @@ async def root(request: Request) -> HTMLResponse:
 
 
 @app.post("/uploadfile/")
-async def upload_file(file: UploadFile) -> dict[str, Any]:
-    file_format = ""
-    if file.filename is not None:
-        file_format = file.filename.split(".")[-1]
+async def upload_file(request: Request, file: UploadFile) -> HTMLResponse:
+    file_format = "" if file.filename is None else file.filename.split(".")[-1]
     if file_format not in SupportedFormats:
         raise HTTPException(status_code=415, detail="File format not supported")
-    if file_format == SupportedFormats.PDB.value:
-        content = await file.read()
-        file_like = StringIO(content.decode("utf-8"))
-        structure: Structure = PDBParser(QUIET=True).get_structure(  # type: ignore
-            file.filename, file_like
-        )
-        chains = set([chain.id for model in structure for chain in model])  # type: ignore
-        return {"chains": chains}
-    return {"filename": file.filename}
+    content = await file.read()
+    file_like = StringIO(content.decode("utf-8"))
+    parser = FORMAT_PARSERS[SupportedFormats(file_format)]
+    original_structure: Structure = parser(QUIET=True).get_structure(
+        file.filename, file_like
+    )
+    corase_structure = coarse_modeler.transform_to_coarse_grain(original_structure)
+    context = {
+        "filename": file.filename,
+        "atoms": [],
+        "models": [],
+        "chains": [],
+        "residues": [],
+    }
+    for structure in [original_structure, corase_structure]:
+        context["atoms"].append(sum(1 for _ in structure.get_atoms()))
+        context["chains"].append(sum(1 for _ in structure.get_chains()))
+        context["models"].append(sum(1 for _ in structure.get_models()))
+        context["residues"].append(sum(1 for _ in structure.get_residues()))
+    return TEMPLATES.TemplateResponse(
+        request=request,
+        name="comparison.html",
+        context=context,
+    )
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5050)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=5050)
