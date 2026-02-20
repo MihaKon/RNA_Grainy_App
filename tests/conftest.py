bc9ef0d624daff21ed0182dbab6783d39e8194b9
MihaKon <konon.michal@gmail.com>
2026-02-20 22:08:47 +0100

    Remove html injection and test it

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,41 @@
 import pathlib
+import subprocess
 from io import BytesIO
 
 import pytest
 from fastapi.testclient import TestClient
 from gemmi import Structure, cif, make_structure_from_block
+from playwright.sync_api import Page
 
 from app.main import app
 
 TEST_DATA_DIR = pathlib.Path(__file__).parent / "data"
+PORT = 8000
+BASE_URL = f"http://localhost:{PORT}"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def start_local_server():
+    process = subprocess.Popen(
+        ["uvicorn", "app.main:app", "--port", str(PORT)],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    yield
+
+    process.terminate()
+    process.wait()
+
+
+@pytest.fixture
+def page(page: Page):
+    page.goto(BASE_URL)
+    yield page
+
+
+@pytest.fixture
+def test_data_dir() -> pathlib.Path:
+    return TEST_DATA_DIR
 
 
 @pytest.fixture
---
+++ tests/e2e/test_html_injection.py
@@ -0,0 +1,23 @@
+import pathlib
+
+from playwright.sync_api import Page, expect
+
+
+def test_html_injection_in_custom_models(page: Page, test_data_dir: pathlib.Path):
+    page.locator('[name="rcsb_id"]').fill("1RNA")
+    page.locator('[class="block truncate text-sm text-primary/50"]').click()
+
+    hidden_item = page.locator('[x-show="dropdownOpen"]').get_by_text("Custom Model")
+    hidden_item.click()
+
+    file_path = test_data_dir / "html_inject_custom_model.json"
+    with page.expect_file_chooser() as fc_info:
+        page.get_by_role("button", name="Upload JSON").click()
+
+    file_chooser = fc_info.value
+    file_chooser.set_files(file_path)
+    page.get_by_text("Process Structure").click()
+
+    model_btn = page.get_by_role("button", name="Model description")
+    expect(model_btn).to_be_visible()
+    model_btn.click()
