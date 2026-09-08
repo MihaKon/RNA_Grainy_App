eb5c4a2cc1c509b7d42787bc510dc0c7ee662529
serguppp <serg.urbaniak@gmail.com>
2026-09-08 03:25:29 +0200

    Bump versions

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ from app.models.form import SupportedFormats
 from app.routes import about, docs, results, uploads
 from app.settings import STATIC_DIR, TEMPLATES
 
-app = FastAPI(title="RNA Coarse Grain App", version="0.1.0")
+app = FastAPI(title="RNAgrainy", version="1.1.0")
 app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
 app.add_middleware(GZipMiddleware)
 app.include_router(about.router)
