91c45452a96690de16648c3d9ba8025340069bd6
serguppp <serg.urbaniak@gmail.com>
2026-09-11 12:50:31 +0200

    Update PDB audit structure

---
+++ pdb_audit/audit.py
@@ -0,0 +1,5 @@
+RCSB_SEARCH_URL = "https://search.rcsb.org/rcsbsearch/v2/query"
+RCSB_FILE_URL = "https://files.rcsb.org/download/{pdb_id}.cif"
+
+DEFAULT_DOWNLOAD_LIMIT_BYTES = 512 * 1024 * 1024
+DEFAULT_PAGE_SIZE = 1_000
